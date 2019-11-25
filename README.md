39a0f668d8a08702db05a1c5ba0dd2ed0bd4858d
Brian Matthews <brian@btmatthews.com>
2019-11-25 07:58:53 +0300

    Use command line parameters

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 Demonstrate invoking a JavaScript function via the Polyglot API.
 ```
-# sdk use java 19.2.0-grl
-# mvn clean install
-# java -jar target/demo-polyglot-js-fibonacci-1.0.0.jar 50
+# sdk use java 19.3.0.r11-grl
+
+# mvn clean package
+
+# java -jar target/polyglot-fibonacci-1.0.0.jar 10
+js.fibonacci(10)=55
+python.fibonacci(10)=55
+R.fibonacci(10)=55
 ``` 
--- src/main/java/com/buralo/training/demo/polyglot/fibonacci/Main.java
+++ src/main/java/com/buralo/training/demo/polyglot/fibonacci/Main.java
@@ -6,26 +6,31 @@ import org.graalvm.polyglot.Value;
 import org.graalvm.polyglot.management.ExecutionListener;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            runScript("js", "/fibonacci.js");
-            runScript("python", "/fibonacci.py");
-            //runScript("ruby", "/fibonacci.rb");
-            runScript("R", "/fibonacci.R");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Arrays.stream(args)
+                .map(Integer::parseInt)
+                .forEach(n -> {
+                    runScript("js", "/fibonacci.js", n);
+                    runScript("python", "/fibonacci.py", n);
+                    //runScript("ruby", "/fibonacci.rb", n);
+                    runScript("R", "/fibonacci.R", n);
+                });
     }
 
-    private static void runScript(final String language, final String script) throws IOException {
-        final Source source = Source.newBuilder(language, Main.class.getResource(script)).build();
-        try (Context context = Context.newBuilder().allowAllAccess(true).build()) {
-            context.eval(source);
-            final Value fn = context.getBindings(language).getMember("fibonacci");
-            System.out.printf("%s.fibonacci(10)=%d\n", language, fn.execute(10).asInt());
+    private static void runScript(final String language, final String script, final int n)  {
+        try {
+            final Source source = Source.newBuilder(language, Main.class.getResource(script)).build();
+            try (Context context = Context.newBuilder().allowAllAccess(true).build()) {
+                context.eval(source);
+                final Value fn = context.getBindings(language).getMember("fibonacci");
+                System.out.printf("%s.fibonacci(%d)=%d\n", language, n, fn.execute(n).asInt());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
